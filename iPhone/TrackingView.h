13789a4b826d2bb49eddb87d17effb335c309fdc
Al <alpascual@gmail.com>
2012-01-28 14:19:57 -0800

    Improving the Tracker version of the MVP Event

---
+++ iPhone/TrackingView.h
@@ -0,0 +1,13 @@
+//
+//  TrackingView.h
+//  MVPEvents
+//
+//  Created by Albert Pascual on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackingView : UIViewController
+
+@end
